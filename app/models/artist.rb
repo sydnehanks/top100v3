6a3f45cacd4da61f155429b7f4687fc59659aee5
Sydne Hanks <sydnehanks@gmail.com>
2020-12-10 17:41:24 -0700

    added fake song list

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,14 +2,4 @@ class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_many :billboards, through: :songs
 
-#def full_name
- # "#"{self.name}
-#end
-  #def create
-    #render plain: params[:artist.inspect
-
-  
-  #def self.order_rank
-  #  order(:rank)
-  #end
 end
--- app/models/billboard.rb
+++ app/models/billboard.rb
@@ -1,6 +1,5 @@
 class Billboard < ApplicationRecord
  has_many :songs, dependent: :destroy
  has_many :artists, through: :songs
- #def index
- #end
+ 
 end
