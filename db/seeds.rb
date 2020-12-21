2c1981793d6a26f5143c1f44aaebdb329a27792e
Sydne Hanks <sydnehanks@gmail.com>
2020-12-21 08:29:12 -0700

    made songs/index loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,22 @@
 7.times do
   billboard = Billboard.create(
-    title: Faker::Music.genre 
+    title: Faker::Music.genre,
+    details: Faker::Quote.famous_last_words
   )
-
-5.times do 
+  5.times do 
     artist = Artist.create(
       name: Faker::Music.band,
-      #rank: Faker::Number.number(digits: 100),
-      #numAlbs: Faker::Number.number(digits: 10)
+      rank: Faker::Number.within(range: 1..100),
+      numAlbs: Faker::Number.within(range: 1..10)
     )
-
-Song.create(
+    Song.create(
       billboard_id: billboard.id,
       artist_id: artist.id,
-     # genre_id: genre.id,
+      genre: Faker::Music.genre,
+      album: Faker::Music.album,
+      title: Faker::Music::Prince.song,
+      length: Faker::Number.within(range: 1..5)
     )
   end
-
 end
 puts 'Data Seeded'
